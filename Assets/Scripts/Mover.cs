0d2674423e08f9d96ed305f1e0d37dd2a6c933ae
SOMRUERDEE KRUALEE <somruerdee.krua@bulive.net>
2016-02-17 11:03:07 +0700

    CREATING SHOTS

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour 
+{
+	public int speed;
+	void Start () {
+		GetComponent<Rigidbody> ().velocity = transform.forward * speed;
+
+}
+	void Update() {
+	}
+}
